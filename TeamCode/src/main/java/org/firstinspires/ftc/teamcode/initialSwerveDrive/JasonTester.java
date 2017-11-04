894912c8b232f7f9b622680c9c246488a52beecb
Alexandre Eichenberger <alexe@us.ibm.com>
2017-11-04 13:56:34 -0400

    json support (class and test class)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialSwerveDrive/JasonTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initialSwerveDrive/JasonTester.java
@@ -31,7 +31,7 @@ where are the file on the phone?
  */
 
 @TeleOp(name="JasonTester")
-public class JsonTester extends LinearOpMode {
+public class JasonTester extends LinearOpMode {
 
     private SafeJsonReader swerveInfo;
     private double incrementServo0;
@@ -67,10 +67,10 @@ public class JsonTester extends LinearOpMode {
         telemetry.addData("Say", "who is best %s", whoIsBest);
         telemetry.addData("Say", "years in action %d", teamYears);
         telemetry.update();
-        swerveInfo.modifyInt("teamYears", teamYears+1);
-        swerveInfo.modifyDouble("increment3", 10.5);
+        swerveInfo.modifyInt("teamYears", 4);
+        swerveInfo.modifyDouble("increment3", 0.9);
         swerveInfo.modifyBoolean("is9773great", false);
-        swerveInfo.modifyString("whoIsTheBest", "we are all the best");
+        swerveInfo.modifyString("whoIsTheBest", "the very 9773");
         swerveInfo.updateFile();
         waitForStart();
         while (opModeIsActive()) {

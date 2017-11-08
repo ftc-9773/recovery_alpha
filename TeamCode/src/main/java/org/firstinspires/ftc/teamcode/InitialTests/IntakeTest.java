90dce7f041f136889422ad89dc2f612ddc5df4c1
arjun <arjun_dayal@icloud.com>
2017-11-08 09:49:21 -0500

    Changed IntakeTest to not run as TeleOp mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialTests/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialTests/IntakeTest.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.InitialTests;
 
 //import .com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
@@ -23,8 +23,6 @@ import java.util.Locale;
  */
 
 
-@TeleOp(name="IntakeTest", group="SwerveTest with angle")
-//@Disabled
 public class IntakeTest extends LinearOpMode {
 
 

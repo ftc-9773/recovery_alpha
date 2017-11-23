9130e001dc2e987c1e84a96b930a99286ecc5ec3
michaelzhou <michaela10c@qq.com>
2017-11-22 19:05:20 -0500

    Mike's push of Debug

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialTests/CryptoboxNavigationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialTests/CryptoboxNavigationTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.teamcode.HardwareControl.SwerveController;
 import org.firstinspires.ftc.teamcode.Vision.VumarkGlyphPattern;
 
 /**
@@ -12,16 +13,17 @@ import org.firstinspires.ftc.teamcode.Vision.VumarkGlyphPattern;
 
 @Autonomous(name = "Cryptobox Navigation", group="Navigation")
 public class CryptoboxNavigationTest extends LinearOpMode{
-
+SwerveController swerveController;
     @Override
     public void runOpMode() throws InterruptedException {
+        swerveController = new SwerveController(hardwareMap);
 
         waitForStart();
 
         while (opModeIsActive()){
-            VumarkGlyphPattern pattern = new VumarkGlyphPattern();
-            pattern.init(hardwareMap);
+            VumarkGlyphPattern pattern = new VumarkGlyphPattern(hardwareMap);
             RelicRecoveryVuMark mark = pattern.getColumn();
+//            swerveController.pointDirection();
             //robot.moveDistance(mark==RelicRecoveryVuMark.LEFT ? farDist : mark==RelicRecoveryVuMark.CENTER ? midDist : mark==RelicRecoveryVuMark.RIGHT ? nearDist);
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/VumarkGlyphPattern.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/VumarkGlyphPattern.java
@@ -18,10 +18,7 @@ public class VumarkGlyphPattern {
     VuforiaLocalizer vuforia;
     RelicRecoveryVuMark vuMark = null;
 
-    public VumarkGlyphPattern(){
-    }
-
-    public void init(HardwareMap hardwareMap){
+    public VumarkGlyphPattern(HardwareMap hardwareMap){
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
         parameters.vuforiaLicenseKey = "AVnz6or/////AAAAGdJgMmsGkkibrBL0inMjc7t54jDqna5iT9Rxes8KZU9k0cZQzyVZCbu3TRLqlFWiujEO7kX8tNMrqcya8ZcZLE4qebycHhi9ZMtMjs7oeb/g1/3TLizLP7ShiVmMQoiCMNiBHqFElzNyL5t5tPk21drKY+aw7q9aHZVgvY1R+ilPd31KKAFn+K077ympaGwv+ywll9uwvvRvYUdxqDYhkAng8bUK26WoCihPDsf5rnRzY9Y/eNr8hZTZwCc6xx1a04agmXLY2JIZ9/8LmB7nRotFXxYw9xoY40DvmKIwcqV77/kDHZ5QG45lRXtSbVxUcUqL2GgojvxtFCDO7/FeTVZoU2ukbT3lA6XrSJ1QvtfX";
@@ -36,6 +33,8 @@ public class VumarkGlyphPattern {
 
         vuMark = RelicRecoveryVuMark.from(relicTemplate);
     }
+
+
 
     public RelicRecoveryVuMark getColumn(){
         return vuMark;

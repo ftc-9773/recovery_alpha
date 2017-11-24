ca8f823897089ff03f3dacb09a9610e46bde2dec
michaelzhou <michaela10c@qq.com>
2017-11-24 14:53:44 -0500

    Drive to Cryptobox position code complete. Ready to test. Check if cube tray controller position is to Stow.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialTests/CryptoboxNavigationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InitialTests/CryptoboxNavigationTest.java
@@ -1,35 +1,36 @@
-/*package org.firstinspires.ftc.teamcode.InitialTests;
+package org.firstinspires.ftc.teamcode.InitialTests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.teamcode.HardwareControl.CubeTrayController;
+import org.firstinspires.ftc.teamcode.HardwareControl.DriveWithPID;
 import org.firstinspires.ftc.teamcode.HardwareControl.SwerveController;
+import org.firstinspires.ftc.teamcode.PositionTracking.Gyro;
 import org.firstinspires.ftc.teamcode.Vision.VumarkGlyphPattern;
-
-/**
- * Created by michaelzhou on 11/22/17.
- **
 
 @Autonomous(name = "Cryptobox Navigation", group="Navigation")
 public class CryptoboxNavigationTest extends LinearOpMode{
-SwerveController swerveController;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        swerveController = new SwerveController(hardwareMap, );
+
+        VumarkGlyphPattern pattern = new VumarkGlyphPattern(hardwareMap);
+        RelicRecoveryVuMark mark = pattern.getColumn();
+
+        Gyro gyro = new Gyro(hardwareMap);
+        SwerveController swerveController = new SwerveController(hardwareMap, gyro, false);
+        DriveWithPID driver = new DriveWithPID(swerveController, gyro);
+        CubeTrayController cubeTrayController = new CubeTrayController(hardwareMap, null);
 
         waitForStart();
 
         while (opModeIsActive()){
-            VumarkGlyphPattern pattern = new VumarkGlyphPattern(hardwareMap);
-            RelicRecoveryVuMark mark = pattern.getColumn();
-//            swerveController.pointDirection();
-            //robot.moveDistance(mark==RelicRecoveryVuMark.LEFT ? farDist : mark==RelicRecoveryVuMark.CENTER ? midDist : mark==RelicRecoveryVuMark.RIGHT ? nearDist);
+            double dist = mark==RelicRecoveryVuMark.LEFT ? 43.0625 : mark==RelicRecoveryVuMark.CENTER ? 35.5625 : 28.0625;
+            driver.driveStraight(true, 1, 0, dist);
+            cubeTrayController.goToStowPos();
+            cubeTrayController.updateServos();
         }
     }
-
-    //1. Initialize robot object and set robot direction
-    //2. Move certain distance according to Vuforia reading
-    //(3. Release the cub into the designated area)
 }
-*/

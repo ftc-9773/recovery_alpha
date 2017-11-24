04ed6ca44361f72eee0ffe6a01e0586765ebc667
michaelzhou <michaela10c@qq.com>
2017-11-24 15:52:20 -0500

    commented out unused code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/VumarkGlyphPattern.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/VumarkGlyphPattern.java
@@ -17,7 +17,6 @@ public class VumarkGlyphPattern {
     VuforiaTrackable template;
     HardwareMap hardwareMap;
     VuforiaLocalizer vuforia;
-    RelicRecoveryVuMark vuMark = null;
 
     public VumarkGlyphPattern(HardwareMap hardwareMap){
         this.hardwareMap = hardwareMap;

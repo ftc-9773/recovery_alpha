50e23af96a5a778fdcbb7fd4cc09bba88ea9583d
michaelzhou <michaela10c@qq.com>
2017-11-24 17:56:50 -0500

    Ready to test the Ball IMAGE DETECTION code!!!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/JewelColorDetectionTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Vision;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+/**
+ * Created by michaelzhou on 11/24/17.
+ */
+@Autonomous(name="Jewel Color Detection", group="Vision")
+public class JewelColorDetectionTest extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        while(opModeIsActive()){
+
+        }
+    }
+}

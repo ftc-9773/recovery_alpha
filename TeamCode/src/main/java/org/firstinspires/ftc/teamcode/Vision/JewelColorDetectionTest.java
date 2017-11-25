aa63d7518c9e4f30822179e7831c384ef894cd88
michaelzhou <michaela10c@qq.com>
2017-11-24 20:24:17 -0500

    bitmap from file is null.... File does not exist for some reason...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/JewelColorDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/JewelColorDetectionTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.Log;
 
 import java.util.ArrayList;
 
@@ -19,21 +20,26 @@ public class JewelColorDetectionTest extends LinearOpMode{
 
         while(opModeIsActive()){
 //            for(String s : )
-            String fileName = "/Users/michaelzhou/Desktop/image recognition files/ball images/b1.jpg";
+            String fileName = "/Users/michaelzhou/Desktop/ball images/b1.jpg";
             JewelColorDetection obj = new JewelColorDetection(fileName);
+//            try{
             telemetry.addData("verdict 1: ", obj.analyze());
+//            }catch (OutOfMemoryError e){
+//                Log.e("ftc9773: ", "Size is too big!!!");
+//            }
 
-            String fileName2 = "/Users/michaelzhou/Desktop/image recognition files/ball images/b3.jpg";
+
+            String fileName2 = "/Users/michaelzhou/Desktop/ball images/b3.jpg";
             JewelColorDetection obj2 = new JewelColorDetection(fileName2);
-            telemetry.addData("verdict 3: ", obj.analyze());
+            telemetry.addData("verdict 3: ", obj2.analyze());
 
-            String fileName3 = "/Users/michaelzhou/Desktop/image recognition files/ball images/b4.jpg";
+            String fileName3 = "/Users/michaelzhou/Desktop/ball images/b4.jpg";
             JewelColorDetection obj3 = new JewelColorDetection(fileName3);
-            telemetry.addData("verdict 4: ", obj.analyze());
+            telemetry.addData("verdict 4: ", obj3.analyze());
 
-            String fileName4 = "/Users/michaelzhou/Desktop/image recognition files/ball images/b9.jpg";
+            String fileName4 = "/Users/michaelzhou/Desktop/ball images/b9.jpg";
             JewelColorDetection obj4 = new JewelColorDetection(fileName4);
-            telemetry.addData("verdict 9: ", obj.analyze());
+            telemetry.addData("verdict 9: ", obj4.analyze());
 
             telemetry.update();
         }

85a3557472e90258daf0fa36c8caa80fde13cd92
Alexandre Eichenberger <alexe@us.ibm.com>
2017-11-25 16:00:28 -0500

    added comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/infrastructure/Instrumentation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/infrastructure/Instrumentation.java
@@ -13,6 +13,60 @@ import java.util.Date;
  * Created by robocracy on 11/11/17.
  */
 
+/*
+        * User interface:
+        *
+        * Instrumentation is by default on (unless the ENABLED flag is set to false here,
+        * in which case nothing happens.
+        *
+        * A given instrumentation object can be enabled and disabled programatically. Use the
+        * enable/disable methods to do so.
+        *
+        * In the main opmode loop, call nextLoopIteration exactly once at the beginning to allow the
+        * instrumentation to relate statistics to a given iteration loop count.
+        *
+        *  To see how an actual instrumentation is used, check out documentation for InstrumentDoubleArray
+        *
+        *
+        *
+        * How to create a new instrumentation subclass:
+        *
+        * Create a subclass, and in the constructor pass the file name to the super-class.
+        * The subclass constructor should also push a string with comma separated header.
+        *
+        * Then create a method that pushes a new statistic, giving the new stats to write,
+        * or having saved in the object enough data to retrieve the data by itself.
+        * The pushing method should detect if the stats is the same as before, in which case
+        * we suggest that the identical value not be pushed... as otherwise the log becomes
+        * with identical values.
+        * If a new stat is seen, then you write it by calling the Instrumentation.Write***
+        *    The writeFullLine function writes the whole stat in one go, expecting a single string of
+        *    comma separated values.
+        *    Otherwise, one can push one value at a time (as a string) using the writeElement
+        *    each individual value. The line of stat is terminated by a writeElementDone
+        *  Instrumentation class will automatically add a time stamp and a count corresponding to
+ *  the main loop.
+         *  The closing of the stats is used to close the file. Empty files are automatically destroyed.
+         *
+         *  To see how an actual instrumentation is defined, check out documentation for InstrumentDoubleArray
+         */
+
+/*
+  where are the file on the phone?
+          storage / emulated / FIRST / team9773 / log18
+
+          1) open terminal tab on android studio
+
+          2) get to the right dir on the computer, for example
+          cd Download
+
+          3) get a file from the phone
+          adb pull /sdcard/FIRST/team9773/log18/filename.csv
+
+          location of adb on mac: $HOME/Library/Android/sdk/platform-tools
+          where you can get the $HOME value by typing "echo $HOME" in a terminal
+*/
+
 public class Instrumentation{
     private static final String baseDir = "/sdcard/FIRST/team9773/log18"; // must end with a name
     private static final String timeStamp;

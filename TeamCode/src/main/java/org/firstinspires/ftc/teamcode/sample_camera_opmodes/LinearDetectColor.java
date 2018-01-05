1e19fde4eb4758bde74c2e8dc96d3c917eee273e
Alexandre Eichenberger <alexe@us.ibm.com>
2018-01-04 21:09:59 -0500

    simplify the interface for jewel detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sample_camera_opmodes/LinearDetectColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sample_camera_opmodes/LinearDetectColor.java
@@ -35,7 +35,7 @@ public class LinearDetectColor extends LinearOpModeCamera {
 
         while (opModeIsActive()) {
 
-            detector.onOffThreshold(true);
+            detector.computeJewelColor();
             telemetry.addData("vuMark", mark);
             telemetry.update();
         }

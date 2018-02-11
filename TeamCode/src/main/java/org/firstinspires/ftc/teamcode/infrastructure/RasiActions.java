4622dcef251bba9e6c62572a752d7e5d1d601cff
Nicolas Eichenberger <nicolas.eichen@gmail.com>
2018-02-10 23:31:16 -0500

    Competition Code!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/infrastructure/RasiActions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/infrastructure/RasiActions.java
@@ -55,8 +55,8 @@ public class RasiActions {
                     ftcRobot.myDriveWithPID.driveTime(rasiParser.getAsDouble(1), rasiParser.getAsDouble(2), rasiParser.getAsDouble(3));
                     break;
                 case "intkl":
-                    timer2 = new Timer(1);
-                    ftcRobot.myRelicSystem.runToPosition(300);
+                    timer2 = new Timer(1.3);
+                    ftcRobot.myRelicSystem.runToPosition(400);
                     while(!timer2.isDone()&&linearOpModeCamera.opModeIsActive()){}
                     ftcRobot.myRelicSystem.runToPosition(0);
                     break;

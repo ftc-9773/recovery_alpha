63fb807b3f5f3d32638e1594c014bb4119dc94e6
Nicolas Eichenberger <nicolas.eichen@gmail.com>
2018-02-19 13:07:28 -0500

    initial ork on the jeel arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareControl/JewelKnocker.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.HardwareControl;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by nicky on 2/19/18.
+ */
+
+public class JewelKnocker {
+
+    private Servo armServo;
+    private Servo knockerServo;
+
+    private static final double armStoredPos = 0.0;
+    private static final double armOutPos = .6;
+    private static final double knockerOutPos= .6;
+    private static final double knockerRightSidePos= 1;
+    private static final double knockerLeftSideStowedPos= 0;
+
+
+    public JewelKnocker(HardwareMap hwMap) {
+
+        armServo = hwMap.servo.get("jaServo");
+        knockerServo = hwMap.servo.get("jkServo");
+
+
+
+    }
+    private void ArmInitialLower() { armServo.setPosition(armOutPos);}
+    private void ArmReturn(){ armServo.setPosition(armStoredPos);}
+    private void KnockerStartMove(){knockerServo.setPosition(knockerOutPos);}
+    private void KnockerLeftStowed(){knockerServo.setPosition(knockerLeftSideStowedPos);}
+    private void KnockerRight(){knockerServo.setPosition(knockerRightSidePos);}
+
+
+
+
+
+}

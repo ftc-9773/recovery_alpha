593d4cbda9fec376570485b523e70469970d2a87
Vikesh Subra <vikesh.bubbles@gmail.com>
2018-03-05 03:49:50 -0500

    began work on new rasi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RASI/RasiParserV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RASI/RasiParserV2.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.RASI;
 
+import org.firstinspires.ftc.robotcontroller.for_camera_opmodes.LinearOpModeCamera;
 import org.firstinspires.ftc.teamcode.infrastructure.FileRW;
 import org.firstinspires.ftc.teamcode.infrastructure.RasiParser;
 
@@ -14,6 +15,8 @@ import java.util.Scanner;
  */
 
 public class RasiParserV2 {
+    private LinearOpModeCamera linearOpModeCamera;
+
     private File rasiFile;                  //File object for the rasi file
     private Scanner fileScanner;            //Scanner object to read the file line by line
     private StringBuilder commandBuilder;   //StringBuilder object for miscellaneous manipulation
@@ -25,8 +28,11 @@ public class RasiParserV2 {
     private String[] TAGS;
     private String[] reservedCommands;
     private boolean shouldExecute = false;
+    private boolean isReservedCommand;
 
-    public RasiParserV2(String filepath, String filename){
+    public RasiParserV2(String filepath, String filename, LinearOpModeCamera linearOpModeCamera){
+
+        this.linearOpModeCamera = linearOpModeCamera;
 
         //Make sure file extension is rasi
         if(filename.split(".")[1].toLowerCase() == "rasi"){
@@ -40,7 +46,7 @@ public class RasiParserV2 {
         }
     }
 
-    public void getNextCommand(){
+    private void loadNextCommand(){
         currentCommand = fileScanner.nextLine();
         commandBuilder = new StringBuilder(currentCommand);
 
@@ -58,9 +64,41 @@ public class RasiParserV2 {
         parameters = currentCommand.split(":")[1].split(",");
         if ((Arrays.asList(TAGS).contains(Tag) || Tag.length() == 0) && !Arrays.asList(reservedCommands).contains(parameters[0])){
             shouldExecute = true;
+            isReservedCommand = false;
         }
-        else{
+        else if(Arrays.asList(reservedCommands).contains(parameters[0])){
             shouldExecute = false;
+            isReservedCommand = true;
+        }
+
+        if(isReservedCommand){
+            runReservedCommand(parameters[0]);
+        }
+    }
+    public String getCommand(){
+        loadNextCommand();
+        while(!shouldExecute && linearOpModeCamera.opModeIsActive()) {
+            loadNextCommand();
+        }
+        return parameters[0];
+    }
+    public String getParam(int paramNumber){
+        return parameters[paramNumber];
+    }
+
+    public void runReservedCommand(String command){
+        switch (command){
+            case "changeTags":
+                TAGS = new String[parameters.length-1];
+                for(int n = 0; n < TAGS.length; n++){
+                    TAGS[n] = parameters[n+1];
+                }
+                break;
+
+            case "end":
+                linearOpModeCamera.requestOpModeStop();
+                while(linearOpModeCamera.opModeIsActive()){}
+                break;
         }
     }
 }

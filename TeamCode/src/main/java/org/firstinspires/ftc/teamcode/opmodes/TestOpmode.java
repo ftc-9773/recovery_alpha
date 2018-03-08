f9923b007df865188309e3dfad8b1a3483c4d6ed
Nicolas Eichenberger <nicolas.eichen@gmail.com>
2018-03-08 13:28:20 -0500

    Fixed problems with previous push - drive until it picks up cubes and using ultrasonic sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestOpmode.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.robotcontroller.for_camera_opmodes.LinearOpModeCamera;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.HardwareControl.DistanceColorSensor;
 import org.firstinspires.ftc.teamcode.HardwareControl.FTCrobot;
 /*
  * Created by zacharye on 11/19/17.
@@ -17,18 +19,27 @@ import org.firstinspires.ftc.teamcode.HardwareControl.FTCrobot;
 public class TestOpmode extends LinearOpModeCamera {
 
     private FTCrobot ftcRobot;
+    private DistanceColorSensor distanceSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("Init:", "waiting...");
         telemetry.update();
-        ftcRobot = new FTCrobot(hardwareMap, telemetry, gamepad1, gamepad2, this);
+        //ftcRobot = new FTCrobot(hardwareMap, telemetry, gamepad1, gamepad2, this);
+        distanceSensor = new DistanceColorSensor(hardwareMap, "sensor");
 
 
         telemetry.addData("Init", "Successful!!");
         telemetry.update();
 
         waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Sensor Reading", distanceSensor.getDistance(DistanceUnit.INCH));
+            if (distanceSensor.getDistance(DistanceUnit.INCH) < 2.35) telemetry.addData("Cube", "Present");
+            else telemetry.addData("Cube", "Absent");
+            telemetry.update();
+        }
     }
 
 }
